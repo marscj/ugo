498a7e12cf78557aa35e2b82c39a48e8ccf462d0
marscj <mjx_cj@163.com>
2019-07-01 19:32:57 +0400

    commit

--- ugo/backend/app/authorization/serializers.py
+++ ugo/backend/app/authorization/serializers.py
@@ -1,8 +1,32 @@
 from rest_framework import  serializers
 
-from .models import CustomUser
+from .models import CustomUser, Role, Permission, ActionEntity
+
+class ActionEntitySerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = ActionEntity
+        fields = '__all__'
+
+class PermissionSerializer(serializers.ModelSerializer):
+
+    actionEntitySet = ActionEntitySerializer(many=True)
+
+    class Meta:
+        model = Permission
+        fields = '__all__'
+
+class RoleSerializer(serializers.ModelSerializer):
+
+    permissions = PermissionSerializer(many=True)
+
+    class Meta:
+        model = Permission
+        fields = '__all__'
 
 class UserSerializer(serializers.ModelSerializer):
+
+    role = RoleSerializer(many=False)
 
     class Meta:
         model = CustomUser
