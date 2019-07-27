4023f6e3f0fed86dacaef696368e65b4a4c91188
marscj <mjx_cj@163.com>
2019-07-27 09:06:24 +0400

    commit

--- ugo/backend/app/order/models.py
+++ ugo/backend/app/order/models.py
@@ -1,12 +1,11 @@
 from django.db import models
 
+
 class Order(models.Model):
     
     day = models.DateField(blank=True, null=True)
 
     start_time = models.TimeField(blank=True, null=True)
-
-    end_time = models.TimeField(blank=True, null=True)
 
     create_at = models.DateTimeField(auto_now_add=True)
 
