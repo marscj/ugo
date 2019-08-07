904ccc97887b22ebcefaee9a9eddfec393ea9bc9
marscj <mjx_cj@163.com>
2019-08-07 18:21:27 +0400

    commit

--- backend/backend/settings/settings.py
+++ backend/backend/settings/settings.py
@@ -140,7 +140,7 @@ CACHEOPS = {
 # 跨域
 CORS_ALLOW_CREDENTIALS = True
 CORS_ORIGIN_ALLOW_ALL = True
-CORS_ORIGIN_WHITELIST = ('*',)
+# CORS_ORIGIN_WHITELIST = ['http://127.0.0.1:8080', 'http://localhost:8080', 'ugodubai.com']
 CORS_ALLOW_HEADERS = ('*',)
 CORS_ALLOW_METHODS = ('*',)
 
