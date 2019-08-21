e0cf9f4fe7ecc6ac41528bb71e108c65108ffe17
marscj <mjx_cj@163.com>
2019-08-21 14:57:21 +0400

    commit

--- frontend/src/views/home/gift.vue
+++ frontend/src/views/home/gift.vue
@@ -26,7 +26,7 @@ export default {
   methods: {
     fetch(search) {
       this.loading = true
-      getProductList({category: 6, search: search, sorter:'-id,sort_by'}).then((res) => {
+      getProductList({category: 6, search: search, sorter:'sort_by'}).then((res) => {
         const { result } = res
         this.data = result
       }).finally(() => {
