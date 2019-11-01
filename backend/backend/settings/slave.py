867fc15e8d7986dce969346aa022e5be5e4f807a
marscj <mjx_cj@163.com>
2019-11-01 10:13:22 +0400

    commit

---
+++ backend/backend/settings/slave.py
@@ -0,0 +1,47 @@
+from .settings import *
+
+DEBUG = False
+
+ALLOWED_HOSTS = ['ugodubai.com', '*']
+
+# SESSION_COOKIE_DOMAIN = 'ugodubai.com'
+
+SLAVE_MODLE = True
+
+CORS_ORIGIN_ALLOW_ALL = False
+
+CACHEOPS_REDIS = "redis://redis:6379/0"
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'password123',
+        'HOST': 'db',
+        'PORT': 5432,
+    }
+}
+
+JWT_AUTH = {
+    'JWT_EXPIRATION_DELTA': datetime.timedelta(days=30),
+    'JWT_AUTH_COOKIE': 'Access-Token',
+}
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'filters': None,
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+        },
+    },
+}
