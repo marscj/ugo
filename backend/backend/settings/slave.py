e4ceaa256ec986bea05c3ea5d5ee4fe8f7da018e
marscj <mjx_cj@163.com>
2019-11-02 14:50:23 +0400

    commit

--- backend/backend/settings/slave.py
+++ backend/backend/settings/slave.py
@@ -19,17 +19,17 @@ DATABASES = {
         'HOST': '193.112.55.69',
         'PORT': '5433',
     },
-    # 'slave': {
-    #     'ENGINE': 'django.db.backends.postgresql_psycopg2',
-    #     'NAME': 'postgres',
-    #     'USER': 'postgres',
-    #     'PASSWORD': 'password123',
-    #     'HOST': 'localhost',
-    #     'PORT': 5432,
-    # }
+    'slave': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'password123',
+        'HOST': 'localhost',
+        'PORT': 5432,
+    }
 }
 
-# DATABASE_ROUTERS = ['middleware.Router',]
+DATABASE_ROUTERS = ['middleware.router.Router',]
 
 JWT_AUTH = {
     'JWT_EXPIRATION_DELTA': datetime.timedelta(days=30),
