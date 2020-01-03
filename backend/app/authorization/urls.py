60d4f7a14846a5a6b3ca95e61808f03922f09d35
marscj <mjx_cj@163.com>
2020-01-03 14:35:32 +0400

    commit

--- backend/app/authorization/urls.py
+++ backend/app/authorization/urls.py
@@ -7,7 +7,7 @@ router = DefaultRouter()
 router.register(r'user', views.UserView, basename='user')
 router.register(r'role', views.RoleView, basename='role')
 router.register(r'permission', views.PermissionView, basename='permission')
-router.register(r'action', views.ActionEntityView, base_name='actions')
+router.register(r'action', views.ActionEntityView, base_name='action')
 
 urlpatterns = [
     url(r'auth/login', views.LoginJwtTokenView.as_view(), name='login'),
